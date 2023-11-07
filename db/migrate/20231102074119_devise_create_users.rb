542e88c3577fb370416d34da557f85f1d3b5eba3
amajun0202 <chihirokento8888@gmail.com>
2023-11-07 14:21:34 +0900

    Update 20231102074119_devise_create_users.rb

--- db/migrate/20231102074119_devise_create_users.rb
+++ db/migrate/20231102074119_devise_create_users.rb
@@ -4,10 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               
-      t.text   :profile,            
-      t.text   :occupation,         
-      t.text   :position,          
+      t.string :name              
+      t.text   :profile            
+      t.text   :occupation         
+      t.text   :position         
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
