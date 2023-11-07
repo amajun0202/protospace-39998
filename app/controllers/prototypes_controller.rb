bd73120df9d2e15d5248b65d67d680add64726c0
amajun0202 <chihirokento8888@gmail.com>
2023-11-07 16:46:05 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,40 +22,41 @@ class PrototypesController < ApplicationController
   def show
   @prototype = Prototype.find(params[:id])
   @comment = Comment.new
-  @comments = @prototype.comment.includes(:user)
- end
+@comments = @prototype.comment.includes(:user)
+end
 
- def edit
-  @prototype = Prototype.find(params[:id])
- end
-
- def update
-  prototype = Prototype.find(params[:id])
-  if prototype.update(prototype_params)
-    redirect_to prototype_path(prototype.id)
-  else
-    render :edit, status: :unprocessable_entity
-  end  
- end
-
- def destroy
-  prototype = Prototype.find(params[:id])
-  prototype.destroy
+def edit
+ @prototype = Prototype.find(params[:id])
+ if @prototype.user != current_user
   redirect_to root_path
+end
+end
+
+def update
+ prototype = Prototype.find(params[:id])
+ if prototype.update(prototype_params)
+   redirect_to prototype_path(prototype.id)
+ else
+   render :edit, status: :unprocessable_entity
+ end  
+end
+
+def destroy
+ prototype = Prototype.find(params[:id])
+ prototype.destroy
+ redirect_to root_path
+end
+
+ private
+
+ def prototype_params
+   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
  end
 
-
-
-  private
-
-  def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+ def move_to_index
+   unless user_signed_in?
+     redirect_to action: :index
+   end
+ end
 
 end
